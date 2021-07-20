9919887b62f1bef2dd74ce2c457f1c0a085ea186
araujoizabelle <araujoizabelle@users.noreply.github.com>
2021-07-20 22:32:11 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.b87953a9cc658a357f109101682a2f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0f41149931c433e7cf70f91c4b7570",
+    "url": "https://araujoizabelle.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "f5c6b806a4485b443f16",
+    "url": "https://araujoizabelle.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "90725779bc36ff6ce207",
+    "url": "https://araujoizabelle.github.io/task-management-frontend/static/js/2.31c005de.chunk.js"
+  },
+  {
+    "revision": "f5c6b806a4485b443f16",
+    "url": "https://araujoizabelle.github.io/task-management-frontend/static/js/main.e97b2772.chunk.js"
+  },
+  {
+    "revision": "fdd59a9e1bc7389dcc08",
+    "url": "https://araujoizabelle.github.io/task-management-frontend/static/js/runtime~main.afe0f06b.js"
+  }
+]);
